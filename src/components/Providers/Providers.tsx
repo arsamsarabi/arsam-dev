7933ad257fae795a29d4b98c3f1df020563bcff5
Arsam Sarabi <arsamsarabi@me.com>
2024-04-14 13:05:12 +0100

    feat: next-ui & next-themes

---
+++ src/components/Providers/Providers.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import { NextUIProvider } from '@nextui-org/react';
+import { ThemeProvider } from 'next-themes';
+import type { PropsWithChildren } from 'react';
+
+export const Providers = ({ children }: PropsWithChildren<unknown>) => {
+  return (
+    <ThemeProvider attribute="class">
+      <NextUIProvider>{children}</NextUIProvider>
+    </ThemeProvider>
+  );
+};
