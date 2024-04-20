ba744c1ae396f596d15086e447dc213729a4ed00
Arsam Sarabi <arsamsarabi@me.com>
2024-04-20 10:48:28 +0100

    puts env vars in a client component

--- src/components/app-providers/app-providers.tsx
+++ src/components/app-providers/app-providers.tsx
@@ -3,8 +3,11 @@
 import { MantineProvider } from '@mantine/core'
 import type { PropsWithChildren } from 'react'
 import { theme } from '#/styles/theme'
+import { env } from '#/utils/env'
 
 export const AppProviders = ({ children }: PropsWithChildren<unknown>) => {
+  console.log('client side env vars')
+  console.log(env)
   return (
     <MantineProvider defaultColorScheme="auto" theme={theme}>
       {children}
